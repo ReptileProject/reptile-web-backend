75a0543326fdc1ae9de3ce53e9fe4cc42067da8c
yeochi1201 <yeochi1201@gmail.com>
2025-05-02 21:32:16 +0900

    REP-36 feat/creature-dto

---
+++ REPTILE_PROJECT/src/main/java/com/reptile/model/dto/Creature.java
@@ -0,0 +1,25 @@
+package com.reptile.model.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Creature {
+	@Schema(description="id for creature", requiredMode=RequiredMode.REQUIRED)
+	private int creatureId;
+	@Schema(description="creature description", requiredMode=RequiredMode.REQUIRED)
+	private String descript;
+	@Schema(description="creature's icun group", requiredMode=RequiredMode.NOT_REQUIRED)
+	private int icunId;
+	@Schema(description="id for creature's habitat", requiredMode=RequiredMode.REQUIRED)
+	private int regionId;
+	@Schema(description="id for creature's kind", requiredMode=RequiredMode.REQUIRED)
+	private int groupId;
+}
