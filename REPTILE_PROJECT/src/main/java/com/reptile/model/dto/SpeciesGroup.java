fa584078b9a2790a7f9b8fc1917c80486c6b58e3
yeochi1201 <yeochi1201@gmail.com>
2025-05-03 22:41:59 +0900

    feat/species group dto
    REP-36 species group dto create

---
+++ REPTILE_PROJECT/src/main/java/com/reptile/model/dto/SpeciesGroup.java
@@ -0,0 +1,21 @@
+package com.reptile.model.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SpeciesGroup {
+	@Schema(description="Group Id", requiredMode=RequiredMode.REQUIRED)
+	private int groupId;
+	@Schema(description="Group Name", requiredMode=RequiredMode.REQUIRED)
+	private String groupName;
+	@Schema(description="Parent Group Id", requiredMode=RequiredMode.NOT_REQUIRED)
+	private int upperGroupId;
+}
